b79869dd4e1cf05ccb399ca20ebac0c868fbe37c
Pieter Claus <pieter.claus1@gmail.com>
2024-02-20 18:06:26 +0100

    first check for arguments

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 16:06:15 by pclaus            #+#    #+#             */
-/*   Updated: 2024/02/20 17:28:20 by pclaus           ###   ########.fr       */
+/*   Updated: 2024/02/20 18:04:53 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,21 @@
 
 int main(int argc, char **argv)
 {
-   ft_printf("The argc is: %d\n", argc); 
-   ft_printf("Argv[1] is: %s\n", argv[1]); 
+   Node *a;
+   Node *b;
+
+   a = NULL;
+   b = NULL;
+   if (argc == 1 || ((argc == 2) && !(argv[2][0])))
+   {
+      ft_printf("You need to enter arguments\n");
+      return (1);
+   }
+   else
+   {
+      ft_printf("argc is: %d\n", argc);
+      ft_printf("argv[1] is: %s\n", argv[1]);
+   }
+   ft_printf("%p\n", a);
+   ft_printf("%p\n", b);
 }
