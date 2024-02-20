1ca48831cc7043c93da7d3d1386bfa14d5384300
Pieter Claus <pieter.claus1@gmail.com>
2024-02-20 17:49:32 +0100

    compilation works

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 20:09:15 by pclaus            #+#    #+#             */
-/*   Updated: 2024/02/20 17:20:14 by pclaus           ###   ########.fr       */
+/*   Updated: 2024/02/20 17:47:21 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 # include "../libft/includes/libft.h"
 # include "./actions.h"
 
-# include <stdio.h> 
 # include <stdarg.h>
 
 typedef	struct s_stack_node
@@ -25,7 +24,7 @@ typedef	struct s_stack_node
 	int	current_index;
 	struct s_stack_node	*next;
 	struct s_stack_node	*prev;
-}	t_stack_node;
+}						Node;
 
 
 #endif
--- libft/test.c
+++
@@ -1,7 +0,0 @@
-#include "includes/libft.h"
-
-int main(void)
-{
-    ft_printf("The argc is: %d\n", 2);
-    ft_printf("Argv[1] is: %s\n", "hello");
-}
