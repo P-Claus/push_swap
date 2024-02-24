bc973bcf9f59b8928e53a1c1dc16cd1583663bb7
Pieter Claus <pieter.claus1@gmail.com>
2024-02-24 15:02:14 +0100

    print_list function prints from left to right

--- src/print_list.c
+++ src/print_list.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 21:16:02 by pclaus            #+#    #+#             */
-/*   Updated: 2024/02/23 21:28:47 by pclaus           ###   ########.fr       */
+/*   Updated: 2024/02/24 14:59:45 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,26 @@
 
 void    print_list(Node *head_a, Node *head_b, int argc)
 {
-    int count;
-
-    count = 0;
+    (void)argc;
     
     Node *current_a = head_a;
     Node *current_b = head_b;
-    ft_printf("\n_ _\n\n");
-    while (count < argc - 1)
+    while ((current_a != NULL) && (current_a->value != 0))
     {
         if (current_a != NULL && current_a->value != 0)
         {
-            ft_printf("%d", current_a->value);
+            ft_printf("%d   ", current_a->value);
             current_a = current_a->next;
         }
+    }
+    ft_printf("\n");
+    while ((current_b != NULL) && (current_b->value != 0))
+    {
         if (current_b != NULL && current_b->value != 0)
         {
-            ft_printf("%d\n", current_b->value);
+            ft_printf("%d   ", current_b->value);
             current_b = current_b->next;
         }
-        else
-            ft_printf("\n");
-        count++;
     }
-    ft_printf("_ _\n");
-    ft_printf("a b\n");
+    ft_printf("\n");
 }
