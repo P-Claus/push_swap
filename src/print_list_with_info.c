767095f2122bdae40e78ea0a5ef565a09477da22
Pieter Claus <pieter.claus1@gmail.com>
2024-02-25 22:01:23 +0100

    create function to print list with prev and next pointers

---
+++ src/print_list_with_info.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_list_with_info.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/25 21:50:56 by pclaus            #+#    #+#             */
+/*   Updated: 2024/02/25 21:58:47 by pclaus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void    print_list_with_info(Node *head_a, Node *head_b, int argc)
+{
+    (void)argc;
+    
+    Node *current_a = head_a;
+    Node *current_b = head_b;
+    ft_printf("a:   ");
+    while ((current_a != NULL) && (current_a->value != 0))
+    {
+            ft_printf("V: %d | ", current_a->value);
+            if (current_a->next)
+                ft_printf("N: %p | ", current_a->next);
+            if (current_a->prev)    
+                ft_printf("P: %p | ", current_a->prev);
+            current_a = current_a->next;
+    }
+    ft_printf("\n");
+    ft_printf("b:   ");
+    while ((current_b != NULL) && (current_b->value != 0))
+    {
+            ft_printf("V: %d | ", current_b->value);
+            if (current_b->next)
+                ft_printf("N: %p | ", current_b->next);
+            if (current_b->prev)    
+                ft_printf("P: %p | ", current_b->prev);
+            current_b = current_b->next;
+    }
+    ft_printf("\n");
+}
