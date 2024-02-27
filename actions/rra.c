2cf51e96f9dd7f111e4235204e221179047170fe
Pieter Claus <pieter.claus1@gmail.com>
2024-02-27 14:42:38 +0100

    add rra action

---
+++ actions/rra.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rra.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 14:15:25 by pclaus            #+#    #+#             */
+/*   Updated: 2024/02/27 14:40:02 by pclaus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void    rra(Node **head)
+{
+    Node    *old_tail;
+    Node    *current;
+
+    head_to_tail(head);
+    old_tail = *head;
+    *head = (*head)->prev;
+    current = *head;
+    current->next = NULL;
+    while (current->prev != NULL)
+        current = current->prev;
+    current->prev = old_tail;
+    old_tail->next = current;
+    old_tail->prev = NULL;
+    *head = old_tail;
+    ft_printf("rra\n");
+}
