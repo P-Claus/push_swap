27017cd48a59f7e988617de0facee613c6d08f8b
Pieter Claus <pieter.claus1@gmail.com>
2024-02-27 17:50:10 +0100

    change print function to also print when value is 0

--- src/print_list.c
+++ src/print_list.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 21:16:02 by pclaus            #+#    #+#             */
-/*   Updated: 2024/02/25 21:49:30 by pclaus           ###   ########.fr       */
+/*   Updated: 2024/02/27 17:45:19 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ void    print_list(Node *head_a, Node *head_b, int argc)
     Node *current_a = head_a;
     Node *current_b = head_b;
     ft_printf("a:   ");
-    while ((current_a != NULL) && (current_a->value != 0))
+    while ((current_a != NULL))
     {
             ft_printf("%d   ", current_a->value);
             current_a = current_a->next;
     }
     ft_printf("\n");
     ft_printf("b:   ");
-    while ((current_b != NULL) && (current_b->value != 0))
+    while ((current_b != NULL))
     {
         ft_printf("%d   ", current_b->value);
             current_b = current_b->next;
