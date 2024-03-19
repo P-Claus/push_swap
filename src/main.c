b6a6d8635664ef789a9d8c6afe9bfafbbe2dd515
Pieter Claus <pieter.claus1@gmail.com>
2024-03-19 16:31:59 +0100

    test on mac for checker

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 10:43:31 by pclaus            #+#    #+#             */
-/*   Updated: 2024/03/19 15:22:09 by pclaus           ###   ########.fr       */
+/*   Updated: 2024/03/19 16:30:12 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,13 @@ int	main(int argc, char **argv)
 		insert_multiple_end(argv, &a);
 	if (argc == 4)
 		sort_three(&a);
-	if (argc == 6)
-		sort_five(&a, &b);
+	// if (argc == 6)
+	// sort_five(&a, &b);
 	// print_list(a, b);
-	if (argc == 101)
+	if (argc == 6)
 		sort_many(&a, &b);
 	tail_to_head(&a);
-	print_list(a, b);
+	// print_list(a, b);
 	// print_list_with_info(a, b, argc);
 	remove_all_nodes(&a);
 	// remove_all_nodes(&b);
