d815322ccb401274137e4b71c84109d4e97dca6d
Pieter Claus <pieter.claus1@gmail.com>
2024-03-26 13:47:21 +0100

    fix norminette

--- src/sort_four.c
+++ src/sort_four.c
@@ -6,22 +6,22 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 13:23:26 by pclaus            #+#    #+#             */
-/*   Updated: 2024/03/26 13:28:37 by pclaus           ###   ########.fr       */
+/*   Updated: 2024/03/26 13:46:43 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void sort_four(t_node **head_a, t_node **head_b)
+void	sort_four(t_node **head_a, t_node **head_b)
 {
-    int destination;
+	int	destination;
 
-    if (check_if_list_is_sorted(head_a) == 0)
-        return;
-    tail_to_head(head_a);
-    pb(head_a, head_b);
-    sort_three(head_a);
-    destination = check_destination(head_a, (*head_b)->value);
-    sort_five_fourth_int(head_a, head_b, destination);
-    sort_five_final_sort(head_a);
+	if (check_if_list_is_sorted(head_a) == 0)
+		return ;
+	tail_to_head(head_a);
+	pb(head_a, head_b);
+	sort_three(head_a);
+	destination = check_destination(head_a, (*head_b)->value);
+	sort_five_fourth_int(head_a, head_b, destination);
+	sort_five_final_sort(head_a);
 }
